82a2abc8619ec0802f418af5396dd4b74e6e63f0
luizbem007-dot <luizbem007@gmail.com>
2026-03-17 17:02:47 -0300

    style: cor principal alterada de azul para bordô #680d3a

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -44,9 +44,9 @@ export const focusInput = [
   // base
   "focus:ring-2",
   // ring color
-  "focus:ring-blue-200 focus:dark:ring-blue-700/30",
+  "focus:ring-[#680d3a]/20 focus:dark:ring-[#680d3a]/30",
   // border color
-  "focus:border-blue-500 focus:dark:border-blue-700",
+  "focus:border-[#680d3a] focus:dark:border-[#680d3a]",
 ];
 
 // Tremor Raw focusRing [v0.0.1]
@@ -55,7 +55,7 @@ export const focusRing = [
   // base
   "outline outline-offset-2 outline-0 focus-visible:outline-2",
   // outline color
-  "outline-blue-500 dark:outline-blue-500",
+  "outline-[#680d3a] dark:outline-[#680d3a]",
 ];
 
 // Tremor Raw hasErrorInput [v0.0.1]
